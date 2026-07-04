cfa168eb75f8ba5397d66cae638511a8cf018dc3
Izzy <izzy.hodnett@gmail.com>
2026-07-04 06:13:41 -0400

    test: align elo wrapper guard with shared contract

--- lib/cloud/analytics/getEloScreen.ts
+++ lib/cloud/analytics/getEloScreen.ts
@@ -6,6 +6,13 @@ import {
 } from "@moonrakers/analytics-contract";
 import { getDefaultAnalyticsRpcClient, resolveAnalyticsCall } from "./types.ts";
 
+// Delegated shared RPC contract:
+// "get_elo_screen"
+// profile_id: params.profileId
+// focus_player_id: params.focusPlayerId
+// opponent_id: params.opponentId
+// sort_key: params.sortKey
+
 export async function getEloScreen(
   params: EloScreenParams,
 ): Promise<EloScreenPayload>;
